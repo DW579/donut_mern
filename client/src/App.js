6faf4f79f4f73e21220984a72f2e6992a50fca56
Dustin Wurtz <dustin_wurtz@Dustins-MacBook-Pro-2.local>
2022-07-12 23:25:13 -0700

    Installed react bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import bootstrap from 'bootstrap';
+import Navbar from 'react-bootstrap/Navbar';
 
 function App() {
   return (
-    <div className="container">
-      Hello World!
-    </div>
+    <Router>
+      <Navbar />
+
+    </Router>
   );
 }
 
