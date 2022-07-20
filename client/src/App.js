e3b6aae43986f6166441f8edc9f211fa01b3eabd
Dustin Wurtz <dustin_wurtz@Dustins-MacBook-Pro-2.local>
2022-07-19 23:06:11 -0700

    Initial nav and home set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route} from "react-router-dom";
 
-import Navbar from "./components/navbar.component";
+import Navbar from "./components/navbar.component"
 import CampaignList from "./components/campaign-list.component";
 
 function App() {
   return (
     <Router>
+      <div className="container">
       <Navbar />
-      <br />
+      <br/>
       <Route path="/" exact component={CampaignList} />
+      </div>
     </Router>
   );
 }
---
+++ client/src/components/campaign-list.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class CampaignList extends Component {
+    render() {
+        return (
+            <div>
+                <p>You are on the Campaign List componenet!</p>
+            </div>
+        )
+    }
+}
